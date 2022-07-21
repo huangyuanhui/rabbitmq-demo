ae68de42b8c0b68d0b34d84620e708738d7f0fc0
huangyuanhui <2278181701@qq.com>
2022-07-22 00:28:19 +0800

    Work Queue工作消息对列：可以提高消息处理速度，避免消息堆积 例子，如： 一个Publisher 一个Queue 两个Consumer 在RabbitMQ中，消息是阅后即焚，就是说，比如一个消息给到consumer1，consumer1消费掉了，消息就会删除，consumer2是不会拿到这个消息的，那么如果有50条消息，那么消息会分配到两个consumer，共同处理。 为什么要多个consumer？  假设你真的只有一个consumer，而这个consumer的处理速度只有每秒40条，而Publisher每秒却发送50条，那么每秒钟就会多出10条消息没人处理，那这些消息只能堆积在队列当中，队列在内存中有存储上限的，每秒多10条，假以时日，是会把队列堆满，这个时候再有消息就过不来，消息就会被丢弃掉！而如果我有两个Consumer，那么就不会有这个问题
    案例：模拟WorkQueue，实现一个队列绑定多个消费者：
    思路如下：
    1：在Publisher服务中定义测试方法，每秒产生50条消息，发送到simple.queue;
    2：在consumer服务中定义两个消息监听者，都监听simple.queue队列
    3：消费者1每秒处理50条消息，消费者2每秒处理10条消息
    
    消息预取问题
    当有大量的消息到达队列时，队列会把消息进行投递，各个consumer的通道Channel会提前把消息拿过来，就是消息预取，各个consumer的通道Channel会轮流拿消息，不管对应的Consumer能不能处理，先拿过来再说，于是各个消费者就会平均分配所有的消息。但是消费者处理消息速度有快慢，这样子会导致处理消息的时间变长。
    要解决这个问题就是不能让Consumer的Channel拿太多消息，应该是消费者没处理完液体个消息，再去队列拿一个！要配置：listener.simple.prefetch=1（每次只能获取一条消息，处理完成才能获取下一个消息）
    
    WorkQueue模型的使用：
    1：多个消费者绑定到一个队列，同一条消息只会被一个消费者处理；
    2：通过设置prefetch来控制消费者预取的消息数量

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitMQListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitMQListener.java
@@ -3,17 +3,45 @@ package cn.itcast.mq.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 
 @Component
 public class SpringRabbitMQListener {
     /**
+     * 基本消息队列：Hello World
      * 消费者要做的事情是去监听消息，那么Spring已经替我们跟RabbitMQ建立了连接，你唯一要操心的是就是：
      * 你要监听什么队列；
      * 监听到队列要干什么事；
      * @param message 发消息时发的是什么，接收的时候就是什么，spring会帮我们处理
      */
-    @RabbitListener(queues = "simple.queue")
+   /* @RabbitListener(queues = "simple.queue")
     public void listSimpleQueue(String message) {
         System.out.println("消费者接收到simple.queue的消息【" + message +"】");
+    }*/
+
+
+    /**
+     * 工作队列：Work Queue
+     * 消费者1每秒处理50条消息
+     *
+     * @param message
+     */
+    @RabbitListener(queues = "simple.queue")
+    public void listWorkQueue1(String message) throws InterruptedException {
+        System.out.println("消费者 1111111111 接收到消息【" + message + "】" + LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    /**
+     * 工作队列：Work Queue
+     * 消费者2每秒处理10条消息，
+     *
+     * @param message
+     */
+    @RabbitListener(queues = "simple.queue")
+    public void listWorkQueue2(String message) throws InterruptedException {
+        System.err.println("消费者 2222222222 接收到消息【" + message + "】" + LocalTime.now());
+        Thread.sleep(100);
     }
 }
--- publisher/src/test/java/cn/itcast/mq/helloworld/SpringAmqpTest.java
+++ publisher/src/test/java/cn/itcast/mq/helloworld/SpringAmqpTest.java
@@ -17,9 +17,26 @@ public class SpringAmqpTest {
      */
     @Test
     void testSendMessage2SimpleQueue() {
-        String queueName = "simple.queue2";
+        String queueName = "simple.queue";
         String message = "hello, spring amqp";
         // 这段代码没有队列不会自动创建
         rabbitTemplate.convertAndSend(queueName, message);
     }
+
+
+    /**
+     * 工作消息队列：Work Queue
+     */
+    @Test
+    void testSendMessage2WorkQueue() throws InterruptedException {
+        String queueName = "simple.queue";
+        String message = "hello, message_";
+        for (int i = 0; i < 50; i++) {
+            // 这段代码没有队列不会自动创建
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            // 大体模拟1秒发50条消息
+            Thread.sleep(20);
+        }
+    }
+
 }
