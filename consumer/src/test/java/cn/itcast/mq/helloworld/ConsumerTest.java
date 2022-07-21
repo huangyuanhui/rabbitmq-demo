f5c47a28be1c7f15e639124910951f2b995e521d
huangyuanhui <2278181701@qq.com>
2022-07-21 22:31:55 +0800

    基本数据队列的消息发送流程： 1：建立Connection 2：创建channel 3：利用channel声明队列 4：利用channel像队列发送消息
    基本消息队列的消息接收流程：
    1：建立Connection
    2：创建channel
    3：利用channel声明队列
    4：定义consumer的消费行为handleDelivery()
    5：利用channel将消费者与队列绑定

---
+++ consumer/src/test/java/cn/itcast/mq/helloworld/ConsumerTest.java
@@ -0,0 +1,50 @@
+package cn.itcast.mq.helloworld;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消息消费测试
+ */
+public class ConsumerTest {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1.建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        // 1.1.设置连接参数，分别是：主机名、端口号、vhost、用户名、密码
+        factory.setHost("120.77.168.189");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        factory.setUsername("hyh");
+        factory.setPassword("123456");
+        // 1.2.建立连接
+        Connection connection = factory.newConnection();
+
+        // 2.创建通道Channel
+        Channel channel = connection.createChannel();
+
+        // 3.创建队列
+        String queueName = "simple.queue";
+        /**
+         * 问什么明明Publisher已经声明、创建过队列了，这里为什么又要声明、创建一次？
+         * 这是因为生产者和消费者启动的顺序是不确定的，万一消费者先启动，那么找队列就不存在，为了避免这种事
+         * 发生，Publisher、Consumer都会去声明队列，但是是不会重复创建队列的
+         */
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        // 4.订阅消息
+        channel.basicConsume(queueName, true, new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                // 5.处理消息，会发现在管理平台消息没了，也就是说只要消息一旦被消费掉，消息会立即被删除，即所谓的而阅后即焚，
+                // 这是RabbitMQ的机制
+                String message = new String(body);
+                System.out.println("接收到消息：【" + message + "】");
+            }
+        });
+        System.out.println("等待接收消息。。。。");
+    }
+}
---
+++ publisher/src/test/java/cn/itcast/mq/helloworld/PublisherTest.java
@@ -0,0 +1,47 @@
+package cn.itcast.mq.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 消息发送测试
+ */
+public class PublisherTest {
+    @Test
+    public void testSendMessage() throws IOException, TimeoutException {
+        // 1.建立连接
+        ConnectionFactory factory = new ConnectionFactory();
+        // 1.1.设置连接参数，分别是：主机名、端口号、vhost、用户名、密码
+        factory.setHost("120.77.168.189");
+        factory.setPort(5672);
+        factory.setVirtualHost("/");
+        factory.setUsername("hyh");
+        factory.setPassword("123456");
+        // 1.2.建立连接，这行代码执行完，管理平台上Connections界面就有连接进来
+        Connection connection = factory.newConnection();
+
+        // 2.创建通道Channel，这行代码执行完，管理平台上Channels界面就有新的通道被创建
+        Channel channel = connection.createChannel();
+
+        // 3.创建队列
+        String queueName = "simple.queue";
+        // 这行代码执行完，管理平台上Queues界面就有对应的队列创建
+        channel.queueDeclare(queueName, false, false, false, null);
+
+        // 4.发送消息
+        String message = "hello, rabbitmq!";
+        // 这行代码执行完，管理平台上Queues界面点击队列名可以查看对应消息
+        channel.basicPublish("", queueName, null, message.getBytes());
+        System.out.println("发送消息成功：【" + message + "】");
+
+        // 5.关闭通道和连接
+        channel.close();
+        connection.close();
+
+    }
+}
