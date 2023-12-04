8a20d1c8393d3695b44c8c833ba226409445bd4c
huangyuanhui <2278181701@qq.com>
2023-12-05 00:14:54 +0800

    RabbitMQ文档完善

--- consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitMQListener.java
+++ consumer/src/main/java/cn/itcast/mq/listener/SpringRabbitMQListener.java
@@ -19,13 +19,13 @@ public class SpringRabbitMQListener {
      * 消费者要做的事情是去监听消息，那么Spring已经替我们跟RabbitMQ建立了连接，你唯一要操心的是就是：
      * 1：你要监听什么队列；
      * 2：监听到队列要干什么事；
+     *
      * @param message 发消息时发的是什么，接收的时候就是什么，spring会帮我们处理
      */
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String message) {
-        System.out.println("消费者接收到simple.queue的消息【" + message +"】");
+        System.out.println("消费者接收到simple.queue的消息【" + message + "】");
     }
-
 
 
     /**
@@ -150,6 +150,7 @@ public class SpringRabbitMQListener {
 
     /**
      * 消息转换器
+     *
      * @param msg
      */
     @RabbitListener(queues = "object.queue")
